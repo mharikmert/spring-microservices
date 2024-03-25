f6ac17490c8be12de6330cea2925d5942b7b67a9
Mahmut Hilmi Arikmert <hilmi.arikmert@renault.com.tr>
2024-03-25 10:53:59 +0300

    Add api gateway & Update configurations

---
+++ api-gateway/src/test/java/org/services/apigateway/ApiGatewayTests.java
@@ -0,0 +1,13 @@
+package org.services.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
